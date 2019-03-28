96c10a345918c6dc2774ac84ec26f8f64c4856cf
Alexandre Juca <corextechnologies@gmail.com>
2019-03-28 11:44:23 +0100

    Implemented callbacks

--- biometrickit/src/main/java/io/github/alexjuca/biometrickit/BiometricCallbackV28.kt
+++ biometrickit/src/main/java/io/github/alexjuca/biometrickit/BiometricCallbackV28.kt
@@ -1,4 +1,42 @@
+/*
+ * Copyright (C) UseKamba Ltda - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential and will be punished by law
+ * Written by Alexandre Antonio Juca <corextechnologies@gmail.com>
+ *
+ */
+
 package io.github.alexjuca.biometrickit
 
-class BiometricCallbackV28 {
+import android.hardware.biometrics.BiometricPrompt
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+@RequiresApi(Build.VERSION_CODES.P)
+class BiometricCallbackV28: BiometricPrompt.AuthenticationCallback {
+    private var biometricKitCallback: BiometricKitCallback
+
+    constructor(biometricKitCallback: BiometricKitCallback) {
+        this.biometricKitCallback = biometricKitCallback
+    }
+
+    override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+        super.onAuthenticationSucceeded(result)
+        this.biometricKitCallback.onAuthenticationSuccessful()
+    }
+
+    override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+        super.onAuthenticationError(errorCode, errString)
+        this.biometricKitCallback.onAuthenticationError(errorCode, errString)
+    }
+
+    override fun onAuthenticationFailed() {
+        super.onAuthenticationFailed()
+        this.biometricKitCallback.onAuthenticationFailed()
+    }
+
+    override fun onAuthenticationHelp(helpCode: Int, helpString: CharSequence?) {
+        super.onAuthenticationHelp(helpCode, helpString)
+        this.biometricKitCallback.onAuthenticationHelp(helpCode, helpString)
+    }
 }
--- biometrickit/src/main/java/io/github/alexjuca/biometrickit/BiometricKitCallback.kt
+++ biometrickit/src/main/java/io/github/alexjuca/biometrickit/BiometricKitCallback.kt
@@ -9,4 +9,24 @@
 package io.github.alexjuca.biometrickit
 
 interface BiometricKitCallback {
+
+    fun onSdkVersionNotSupported()
+
+    fun onBiometricAuthenticationNotSupported()
+
+    fun onBiometricAuthenticationNotAvailable()
+
+    fun onBiometricAuthenticationPermissionNotGranted()
+
+    fun onBiometricAuthenticationInternalError(error: String)
+
+    fun onAuthenticationFailed()
+
+    fun onAuthenticationCancelled()
+
+    fun onAuthenticationSuccessful()
+
+    fun onAuthenticationHelp(helpCode: Int, helpString: CharSequence?)
+
+    fun onAuthenticationError(errorCode: Int, errString: CharSequence?)
 }
