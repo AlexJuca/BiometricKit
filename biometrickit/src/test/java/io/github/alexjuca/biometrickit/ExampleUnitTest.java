a41f57654da420d850efe8e66253f8d787abd62a
Alexandre Juca <corextechnologies@gmail.com>
2019-03-28 09:12:19 +0100

    Initial Project commit

---
+++ biometrickit/src/test/java/io/github/alexjuca/biometrickit/ExampleUnitTest.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) UseKamba Ltda - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential and will be punished by law
+ * Written by Alexandre Antonio Juca <corextechnologies@gmail.com>
+ *
+ */
+
+package io.github.alexjuca.biometrickit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
