e6b212cef0f5c9ad7fa15c1be2b739535f02dfbb
Alexandre Juca <corextechnologies@gmail.com>
2019-03-28 11:44:03 +0100

    Implemented BiometricKitDialog for API levels lower than 28

--- biometrickit/src/main/java/io/github/alexjuca/biometrickit/BiometricKitDialogV23.kt
+++ biometrickit/src/main/java/io/github/alexjuca/biometrickit/BiometricKitDialogV23.kt
@@ -8,5 +8,77 @@
 
 package io.github.alexjuca.biometrickit
 
-class BiometricKitDialogV23 {
+import android.content.Context
+import android.content.DialogInterface
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.NonNull
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
+class BiometricKitDialogV23 : BottomSheetDialog, View.OnClickListener {
+    private lateinit var mContext: Context
+    private lateinit var title: TextView
+    private lateinit var subTitle: TextView
+    private lateinit var description: TextView
+    private lateinit var instructions: TextView
+    private lateinit var icon: ImageView
+    private lateinit var cancel: TextView
+    private lateinit var callback: BiometricKitCallback
+
+    constructor(context: Context) : super(context) {
+        mContext = context.applicationContext
+        setUpBiometricDialog()
+    }
+
+    constructor(context: Context, biometricKitCallback: BiometricKitCallback) : super(context) {
+        mContext = context.applicationContext
+        callback = biometricKitCallback
+        setUpBiometricDialog()
+    }
+
+    constructor(@NonNull context: Context, theme: Int) : super(context, theme)
+
+    constructor(@NonNull context: Context, cancelable: Boolean, onCancelListener: DialogInterface.OnCancelListener) : super(context, cancelable, onCancelListener)
+
+    fun setUpBiometricDialog() {
+        val view = layoutInflater.inflate(R.layout.biometric_auth_bottom_sheet, null)
+        setContentView(view)
+
+        title = view.findViewById(R.id.title)
+        subTitle = view.findViewById(R.id.subtitle)
+        description = view.findViewById(R.id.description)
+        icon = view.findViewById(R.id.biometric_image)
+        cancel = view.findViewById(R.id.cancel_and_dismiss)
+    }
+
+    fun setTitle(title: String) {
+        this.title.text = title
+    }
+
+    fun setDescription(description: String) {
+        this.description.text = description
+    }
+
+    fun setInstructions(instructions: String) {
+        this.instructions.text = instructions
+    }
+
+    fun setSubtitle(subtitle: String) {
+        this.subTitle.text = subtitle
+    }
+
+    fun setNegativeButton(negativeText: String) {
+        this.cancel.text = negativeText
+    }
+
+
+
+
+    override fun onClick(v: View?) {
+        dismiss()
+        callback.onAuthenticationCancelled()
+    }
+
 }
