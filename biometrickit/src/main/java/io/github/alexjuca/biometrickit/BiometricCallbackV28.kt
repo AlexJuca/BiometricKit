38b83457a403e514e202b2ff9a84145b4bc5a542
Alexandre Juca <corextechnologies@gmail.com>
2019-03-28 11:42:27 +0100

    Added id to cancel button

---
+++ biometrickit/src/main/java/io/github/alexjuca/biometrickit/BiometricCallbackV28.kt
@@ -0,0 +1,4 @@
+package io.github.alexjuca.biometrickit
+
+class BiometricCallbackV28 {
+}
---
+++ biometrickit/src/main/java/io/github/alexjuca/biometrickit/BiometricKitDialogV23.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) UseKamba Ltda - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential and will be punished by law
+ * Written by Alexandre Antonio Juca <corextechnologies@gmail.com>
+ *
+ */
+
+package io.github.alexjuca.biometrickit
+
+class BiometricKitDialogV23 {
+}
